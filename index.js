a16d207e409aa71d5a07453aa6e280841b059c8a
Mehul Bechra <bechra.mehul@gmail.com>
2018-11-20 02:22:13 +0530

    Added tokens handler and updated authentication for users handler

--- index.js
+++ index.js
@@ -78,5 +78,6 @@ const unifiedServer = (req,res) => {
 // Request Router
 const router = {
     'ping' : handlers.ping,
-    'users' : handlers.users
+    'users' : handlers.users,
+    'tokens' : handlers.tokens
 };
